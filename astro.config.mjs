8680732fd3a7d7239fffb89e4eb5acfd875de6e8
Vipul Tehri <VTehri@eagle.org>
2026-04-29 04:22:33 -0500

    build changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), react()],
-    security: {
-        csp: true,
-    },
+  site: "https://Vipul-Tehri.github.io",
+  base: "/girlsmantraa",
+  integrations: [tailwind(), react()],
+//   security: {
+//     csp: true,
+//   },
 });
